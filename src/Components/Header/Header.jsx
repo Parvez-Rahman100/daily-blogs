4015e6bc376aad5cfd1f7f21195aaf8d9525f70d
Parvez-Rahman100 <iamparvezrahman@gmail.com>
2022-06-28 17:35:23 +0600

    header section done

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import './header.css';
+import blog from '../../assests/blog.jpg';
 
 const Header = () => {
     return (
-        <div>
-            
+        <div className='header'>
+            <div className="header-titles">
+                <span className='headerTitleLg'>Daily Blogs</span>
+            </div>
+            <img className='headerImg' src={blog} alt="" />
         </div>
     );
 };
--- src/Components/Pages/Home/Home.jsx
+++ src/Components/Pages/Home/Home.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
+import Blogs from '../../Blogs/Blogs';
 import Header from '../../Header/Header';
+import SideBar from '../../SideBar/SideBar';
 import './home.css';
 
 const Home = () => {
     return (
+        <>
+        <Header/>
         <div className='home'>
-            <Header/>
+           <Blogs/>
+           <SideBar/> 
         </div>
+        </>
     );
 };
 
